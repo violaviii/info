9b9a96e774cf9c7e6022c51587d57969ac61f3ff
violaviii <violavitolo.spotify@gmail.com>
2025-05-10 19:53:00 +0200

    5

---
+++ progetto.js
@@ -0,0 +1,8 @@
+
+    function openModal() {
+        document.getElementById("modal").style.display = "flex";
+    }
+
+    function closeModal() {
+        document.getElementById("modal").style.display = "none";
+    }
