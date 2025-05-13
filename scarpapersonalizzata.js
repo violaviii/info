30375584c5a340a027ac0d8da2fc0999ff2ff35c
violaviii <violavitolo.spotify@gmail.com>
2025-05-13 12:52:53 +0200

    g

---
+++ scarpapersonalizzata.js
@@ -0,0 +1,12 @@
+function changeColor() {
+    document.getElementById('scarpa').src= "IMMAGINI/scarpagiallap.png" // Cambia l'immagine in base al colore
+}
+function changeColor1() {
+    document.getElementById('scarpa').src= "IMMAGINI/scarpablup.png" // Cambia l'immagine in base al colore
+}
+function changeColor2() {
+    document.getElementById('scarpa').src= "IMMAGINI/scarparosap.png" // Cambia l'immagine in base al colore
+}
+function changeColor3() {
+    document.getElementById('scarpa').src= "IMMAGINI/scarpaverdep.png" // Cambia l'immagine in base al colore
+}
