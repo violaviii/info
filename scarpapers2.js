bcd9e0c04eae8cf0969ce6dc37587e31f577c59e
Marco3017 <fregni.dallari@gmail.com>
2025-05-24 15:49:09 +0200

    hnyg
    yhry tr

---
+++ scarpapers2.js
@@ -0,0 +1,20 @@
+function changeColor() {
+    document.getElementById('scarpa').src= "IMMAGINI/scarpagiallap.png" // Cambia l'immagine in base al colore
+}
+function changeColor1() {
+    document.getElementById('scarpa').src= "IMMAGINI/scarpablup.png" // Cambia l'immagine in base al colore
+}
+function changeColor2() {
+    document.getElementById('scarpa').src= "IMMAGINI/scarparosap.png" // Cambia l'immagine in base al colore
+}
+function changeColor3() {
+    document.getElementById('scarpa').src= "IMMAGINI/scarpaverdep.png" // Cambia l'immagine in base al colore
+}
+
+function mostraGrazieAcquisto() {    //serve per mostrare il form per l'acquisto
+    document.getElementById('modal').style.display = 'flex';
+}
+
+function closeModal() {  //per chiudere il form di registrazione per l'acquisto
+    document.getElementById('modal').style.display = 'none';
+}
